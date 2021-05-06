766621167fc9c09525b551612797822491115098
jingyu <universeofjimmy@gmail.com>
2021-05-06 10:58:00 +0800

    feat: log gpu status info

--- nvml_client.go
+++ nvml_client.go
@@ -50,8 +50,10 @@ func (nc *NvmlClient) Init() error {
 }
 
 func (nc *NvmlClient) LogMetrics(node string) {
-	// device info
+	now := time.Now()
+
 	for i, device := range nc.Devices {
+		// device info
 		tag := map[string]string{
 			"node":  node,
 			"GPU":   fmt.Sprint(i),
@@ -64,7 +66,24 @@ func (nc *NvmlClient) LogMetrics(node string) {
 			"clock_cores":  fmt.Sprintf("%d MHz", *device.Clocks.Cores),
 			"clock_memory": fmt.Sprintf("%d MHz", *device.Clocks.Memory),
 		}
-		(&MetricsLog{"gpu_device_info", tag, nil, time.Now(), field}).Log()
+		(&MetricsLog{"gpu_device_info", tag, nil, now, field}).Log()
+
+		// GPU usage
+		st, err := device.Status()
+		if err != nil {
+			continue
+		}
+		tag = map[string]string{
+			"node": node,
+			"GPU":  fmt.Sprint(i),
+			"UUID": device.UUID}
+		(&MetricsLog{"gpu_power", tag, *st.Power, now, nil}).Log()
+		(&MetricsLog{"gpu_temperature", tag, *st.Temperature, now, nil}).Log()
+		(&MetricsLog{"gpu_util", tag, *st.Utilization.GPU, now, nil}).Log()
+		(&MetricsLog{"gpu_memory_util", tag, *st.Utilization.Memory, now, nil}).Log()
+		// fmt.Printf("%5d %5d %5d %5d %5d %5d %5d %5d %5d\n",
+		// 	i, *st.Power, *st.Temperature, *st.Utilization.GPU, *st.Utilization.Memory,
+		// 	*st.Utilization.Encoder, *st.Utilization.Decoder, *st.Clocks.Memory, *st.Clocks.Cores)
 	}
 }
 
